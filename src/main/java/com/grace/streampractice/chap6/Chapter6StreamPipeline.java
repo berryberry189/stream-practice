25e6a2d20f0096cb09866e18ff89a8eb4ead58e0
berryberry189 <berryberry189@naver.com>
2022-03-02 21:02:13 +0900

    stream pipeline 간단 예제

---
+++ src/main/java/com/grace/streampractice/chap6/Chapter6StreamPipeline.java
@@ -0,0 +1,90 @@
+package com.grace.streampractice.chap6;
+
+import com.grace.streampractice.chap6.model.Order;
+import com.grace.streampractice.chap6.model.User;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Chapter6StreamPipeline {
+
+    public static void main(String[] args) {
+        User user1 = new User()
+                .setId(101)
+                .setName("Alice")
+                .setVerified(true)
+                .setEmailAddress("alice@email.com");
+        User user2 = new User()
+                .setId(102)
+                .setName("Bob")
+                .setVerified(false)
+                .setEmailAddress("bob@email.com");
+        User user3 = new User()
+                .setId(103)
+                .setName("Charlie")
+                .setVerified(false)
+                .setEmailAddress("charlie@email.com");
+        List<User> users = Arrays.asList(user1, user2, user3);
+
+        // Stream Pipeline
+        List<String> emails = new ArrayList<>();
+        for(User user : users){
+            if(!user.isVerified()){
+                String email = user.getEmailAddress();
+                emails.add(email);
+            }
+        }
+        System.out.println(emails);
+
+        List<String> emails2 = users.stream()
+                .filter(user -> !user.isVerified())
+                .map(User::getEmailAddress)
+                .collect(Collectors.toList());
+        System.out.println(emails2);
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        Order order1 = new Order()
+                .setId(101)
+                .setStatus(Order.OrderStatus.CREATED)
+                .setCreatedByUserId(101)
+                .setCreatedAt(now.minusHours(4));
+        Order order2 = new Order()
+                .setId(102)
+                .setStatus(Order.OrderStatus.ERROR)
+                .setCreatedByUserId(103)
+                .setCreatedAt(now.minusHours(40));
+        Order order3 = new Order()
+                .setId(103)
+                .setStatus(Order.OrderStatus.PROGRESSED)
+                .setCreatedByUserId(102)
+                .setCreatedAt(now.minusHours(30));
+        Order order4 = new Order()
+                .setId(104)
+                .setStatus(Order.OrderStatus.IN_PROGRESS)
+                .setCreatedByUserId(104)
+                .setCreatedAt(now.minusHours(15));
+        Order order5 = new Order()
+                .setId(105)
+                .setStatus(Order.OrderStatus.ERROR)
+                .setCreatedByUserId(101)
+                .setCreatedAt(now.minusHours(20));
+        List<Order> orders = Arrays.asList(order1, order2, order3, order4, order5);
+
+        List<Long> errorStatusUserIds = orders.stream()
+                .filter(order -> order.getStatus() == Order.OrderStatus.ERROR)
+                .map(Order::getCreatedByUserId)
+                .collect(Collectors.toList());
+        System.out.println(errorStatusUserIds);
+
+        List<Order> orderInErrorStatusIn24hrs = orders.stream()
+                .filter(order -> order.getStatus() == Order.OrderStatus.ERROR)
+                .filter(order -> order.getCreatedAt().isAfter(now.minusHours(24)))
+                .collect(Collectors.toList());
+        System.out.println(orderInErrorStatusIn24hrs);
+
+    }
+}
